a38d2cae30d91976f08aec32216a1dd403681883
xingwei <wealththu@163.com>
2021-03-26 10:45:28 +0800

    Initial commit

---
+++ compile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+~/code/jemdoc.py   index.jemdoc
+~/code/jemdoc.py   cv.jemdoc
+~/code/jemdoc.py   research.jemdoc
+~/code/jemdoc.py   teaching.jemdoc
+~/code/jemdoc.py   group.jemdoc
+~/code/jemdoc.py   reading.jemdoc
+~/code/jemdoc.py   others.jemdoc
+~/code/jemdoc.py   links.jemdoc
